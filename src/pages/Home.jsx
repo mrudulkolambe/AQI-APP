26922a26bbbf3170094f94f2a9d9854907fe4740
Mrudul Kolambe <mrudulkolambe02@gmail.com>
2024-02-16 14:21:18 +0530

    changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,44 @@
-import React from 'react'
-import logo from '../logo.svg';
+import React, { useEffect, useState } from 'react'
 
 const Home = () => {
-	//  Template Code
+	const [data, setData] = useState("");
+	function initGeolocation() {
+		if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition(success, fail);
+		}
+		else {
+			alert("Sorry, your browser does not support geolocation services.");
+		}
+	}
+	function success(position) {
+		// let position = {
+		// 	coords: {
+
+		// 	}
+		// };
+		// position.coords.latitude = 19.119634
+		// position.coords.longitude = 72.997541
+		let url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${position.coords.longitude},${position.coords.latitude}.json?limit=1&access_token=pk.eyJ1IjoicmVzaGF2ZSIsImEiOiJjbHM0eTllMW4wYzFhMnBvMDhxa3d2b2N5In0.OdOyhUEKTrBy1x7c6olH_w`;
+		getData(url)
+	}
+
+	function fail() {
+		alert("Error")
+	}
+	useEffect(() => {
+		initGeolocation()
+	}, [])
+
+	async function getData(url) {
+		let x = await fetch(url);
+		let y = await x.json();
+		setData(JSON.stringify(y));
+	}
+
 	return (
 		<>
 			<div className='relative bg z-0' >
-				<section className='flex-col Nunito relative z-10 h-screen w-screen overflow-hidden flex items-center justify-center'>
-					<h1 className='text-4xl text-white font-bold -mt-20 mb-20 w-[50vw] text-center'>A complete boilerplate for your next tailwind and React project!</h1>
-					<div className='flex'>
-						<div className='w-[30vw] items-center justify-center flex flex-col'>
-							<img src={logo} className="h-[20vh]" alt="" />
-							<h2 className='text-2xl font-bold text-white text-center'>ReactJs</h2>
-						</div>
-						<div className='w-[30vw] items-center justify-center flex flex-col'>
-							<img className="h-[20vh]" src="https://camo.githubusercontent.com/bcd4bda49ef6cd9537db065920f4f4f6ac670eae0e0adf2c5133c19b319f1574/68747470733a2f2f627261646c632e67616c6c65727963646e2e76736173736574732e696f2f657874656e73696f6e732f627261646c632f7673636f64652d7461696c77696e646373732f302e322e302f313535383034303536333634392f4d6963726f736f66742e56697375616c53747564696f2e53657276696365732e49636f6e732e44656661756c74" alt="" />
-							<h2 className='text-2xl font-bold text-white text-center'>TailwindCSS</h2>
-						</div>
-					</div>
-				</section>
+				{data}
 			</div>
 		</>
 	)
